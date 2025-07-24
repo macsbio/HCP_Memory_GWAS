4b5c1b1d8b0d859ad275c0b98655439ae40ebdda
beekvandaan <120456233+beekvandaan@users.noreply.github.com>
2025-07-24 16:12:46 +0200

    Update 4_gwas.sh

--- 4_gwas.sh
+++ 4_gwas.sh
@@ -1,6 +1,21 @@
 #!bin/bash
-./gcta/gcta64 --bfile merged_plink_qc --grm-sparse merged_plink_qc_spgrm --fastGWA-mlm --pheno pheno_iwrd_residuals.txt --thread-num 10 --out gene_assoc_iwrd_residuals
+./gcta/gcta64 --mlma-loco \
+--bfile merged_plink_qc \
+--grm merged_plink_qc_grm \
+--pheno pheno_iwrd_residuals.txt \
+--out gwas_output/iwrd \
+--thread-num 10
 
-./gcta/gcta64 --bfile merged_plink_qc --grm-sparse merged_plink_qc_spgrm --fastGWA-mlm --pheno pheno_pics_residuals.txt --thread-num 10 --out gene_assoc_pics_residuals
+./gcta/gcta64 --mlma-loco \
+--bfile merged_plink_qc \
+--grm merged_plink_qc_grm \
+--pheno pheno_pics_residuals.txt \
+--out gwas_output/pics \
+--thread-num 10
 
-./gcta/gcta64 --bfile merged_plink_qc --grm-sparse merged_plink_qc_spgrm --fastGWA-mlm --pheno pheno_list_residuals.txt --thread-num 10 --out gene_assoc_list_residuals
+./gcta/gcta64 --mlma-loco \
+--bfile merged_plink_qc \
+--grm merged_plink_qc_grm \
+--pheno pheno_list_residuals.txt \
+--out gwas_output/list \
+--thread-num 10
